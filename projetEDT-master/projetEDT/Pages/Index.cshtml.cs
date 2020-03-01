7c6470fe1d4b6e6ecdd0d093cc8dd97f573d6c2f
Josselin Abel <jos_boss@live.fr>
2020-03-01 16:02:35 +0100

    Ajout emploi tu temps

--- projetEDT-master/projetEDT/Pages/Index.cshtml.cs
+++ projetEDT-master/projetEDT/Pages/Index.cshtml.cs
@@ -1,25 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using projetEDT.Models;
 
 namespace projetEDT.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly projetEDT.Data.ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, projetEDT.Data.ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public void OnGet()
+        public IList<Seance> Seance { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Seance = await _context.Seance
+                .Include(s => s.LUE)
+                .Include(s => s.LaSalle)
+                .ThenInclude(s => s.LeBatiment)
+                .Include(s => s.LeGroupe)
+                .Include(s => s.Type).ToListAsync();
+        }
+
+        /*public void OnGet()
         {
 
-        }
+        }*/
+
     }
 }
